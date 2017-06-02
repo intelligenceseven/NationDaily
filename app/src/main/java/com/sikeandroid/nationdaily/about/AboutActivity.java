3ea5a3a3dcda5c6e58ee1978acd54b69f30e7246
GreenLight <1251191746@qq.com>
2017-06-02 16:29:36 +0800

    优化引导界面图片加载

--- app/src/main/java/com/sikeandroid/nationdaily/about/AboutActivity.java
+++ app/src/main/java/com/sikeandroid/nationdaily/about/AboutActivity.java
@@ -1,12 +1,19 @@
 package com.sikeandroid.nationdaily.about;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import com.sikeandroid.nationdaily.R;
+import com.sikeandroid.nationdaily.guide.PrefManager;
+import com.sikeandroid.nationdaily.guide.WelcomeActivity;
 
 public class AboutActivity extends AppCompatActivity {
+
+  private Button funcIntroduce;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate( savedInstanceState );
@@ -17,5 +24,13 @@ public class AboutActivity extends AppCompatActivity {
     if (actionBar != null) {
       actionBar.setDisplayHomeAsUpEnabled( true );
     }
+    funcIntroduce = (Button) findViewById( R.id.func_introduce );
+    funcIntroduce.setOnClickListener( new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        new PrefManager( AboutActivity.this ).setFirstTimeLaunch( true );
+        startActivity( new Intent( AboutActivity.this, WelcomeActivity.class ) );
+        finish();
+      }
+    } );
   }
 }
